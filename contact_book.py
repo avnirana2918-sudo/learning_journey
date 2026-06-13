88f1b646dfc54ed78f6075177accd0d877c63586
Avni Rana <avnirana2918@gmail.com>
2026-06-13 12:33:06 +0530

    More enhanced version with other features.

--- contact_book.py
+++ contact_book.py
@@ -1,4 +1,90 @@
 import json
+def add_contact(contacts):
+      name = input("Enter Name: ")
+      duplicate = False
+      for contact in contacts:
+             if contact["name"].lower() == name.lower():
+                  duplicate = True
+      if duplicate == True:
+             print("Contact already exists!")
+      else:
+                while True:
+                    phone = input("Enter phone: ")
+                    if phone.isdigit():
+                         break
+                    else:
+                         print("Invalid! Numbers only.")
+
+                email = input("Enter email: ")
+                contact = {"name":name , "phone":phone , "email":email}
+                contacts.append(contact)
+                print("Contact added!")
+                with open("contacts.json", "w") as f:
+                    json.dump(contacts, f)    
+
+def view_contacts(contacts):
+     if len(contacts) == 0:
+            print("No contacts found.")
+     else:
+         for contact in contacts:
+             print("Name: ",contact["name"])
+             print("Phone: ",contact["phone"])
+             print("Email: ",contact["email"])
+             print("----------")
+
+def search_contacts(contacts):
+      search = input("Enter name to search: ")
+      found = False
+      for contact in contacts:
+            if contact["name"].lower() == search.lower():
+                print("Name: ", contact["name"])
+                print("Phone: ", contact["phone"])
+                print("Email: ", contact["email"])
+                found = True
+      if found == False:
+            print("Contact not found.")
+
+def delete_contact(contacts):
+      name = input("Enter name to delete: ")
+      found = False
+      for contact in contacts:
+            if contact["name"].lower() == name.lower():
+                contacts.remove(contact)
+                found = True
+                break
+      if found == True:
+                print("Contact deleted!")
+                with open("contact.json", "w") as f:
+                     json.dump(contacts, f)
+      if found == False:
+                print("Contact not found")
+
+def edit_contact(contacts):
+      search = input("Enter name to search: ")
+      found = False
+      for contact in contacts:
+             if contact["name"].lower() == search.lower():
+                print("Name: ", contact["name"])
+                print("Phone: ", contact["phone"])
+                print("Email: ", contact["email"])
+                found = True
+                existing_contact = contact
+      if found == True:
+            print("Enter details to update or press Enter to keep old")
+            new_name = input("Enter New Name: ")
+            new_phone = input("Enter New phone: ")
+            new_email = input("Enter New email: ")
+            existing_contact["name"]=new_name or existing_contact["name"]
+            existing_contact["phone"]=new_phone or existing_contact["phone"]
+            existing_contact["email"]=new_email or existing_contact["email"]
+            with open("contact.json", "w") as f:
+                     json.dump(contacts, f)
+            print("Contact updated!")
+
+      elif found == False:
+            print("Contact not found.") 
+
+
 try:
      with open("contacts.json", "r") as f:
           contacts = json.load(f)
@@ -18,73 +104,16 @@ while True:
         break
     
     if choice == 1:
-        name = input("Enter Name: ")
-        phone = input("Enter phone: ")
-        email = input("Enter email: ")
-        contact = {"name":name , "phone":phone , "email":email}
-        contacts.append(contact)
-        print("Contact added!")
-        with open("contacts.json", "w") as f:
-             json.dump(contacts, f)
+       add_contact(contacts)
     
     elif choice == 2:
-        if len(contacts) == 0:
-            print("No contacts found.")
-        else:
-            for contact in contacts:
-                print("Name: ",contact["name"])
-                print("Phone: ",contact["phone"])
-                print("Email: ",contact["email"])
-                print("----------")
+        view_contacts(contacts)
 
     elif choice == 3:
-        search = input("Enter name to search: ")
-        found = False
-        for contact in contacts:
-            if contact["name"].lower() == search.lower():
-                print("Name: ", contact["name"])
-                print("Phone: ", contact["phone"])
-                print("Email: ", contact["email"])
-                found = True
-        if found == False:
-            print("Contact not found.")
-
+        search_contacts(contacts)
+       
     elif choice == 4:
-        name = input("Enter name to delete: ")
-        found = False
-        for contact in contacts:
-            if contact["name"].lower() == name.lower():
-                contacts.remove(contact)
-                found = True
-                break
-        if found == True:
-                print("Contact deleted!")
-                with open("contact.json", "w") as f:
-                     json.dump(contacts, f)
-        if found == False:
-                print("Contact not found")
+        delete_contact(contacts)
 
     elif choice == 6:
-         search = input("Enter name to search: ")
-         found = False
-         for contact in contacts:
-             if contact["name"].lower() == search.lower():
-                print("Name: ", contact["name"])
-                print("Phone: ", contact["phone"])
-                print("Email: ", contact["email"])
-                found = True
-                existing_contact = contact
-         if found == True:
-            print("Enter details to update or press Enter to keep old")
-            new_name = input("Enter New Name: ")
-            new_phone = input("Enter New phone: ")
-            new_email = input("Enter New email: ")
-            existing_contact["name"]=new_name or existing_contact["name"]
-            existing_contact["phone"]=new_phone or existing_contact["phone"]
-            existing_contact["email"]=new_email or existing_contact["email"]
-            with open("contact.json", "w") as f:
-                     json.dump(contacts, f)
-            print("Contact updated!")
-
-         elif found == False:
-            print("Contact not found.")
+         edit_contact(contacts)
